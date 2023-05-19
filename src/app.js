782a85f183777d938d1715ebbe160be306b2ca31
istvan033 <112931949+istvan033@users.noreply.github.com>
2023-05-19 09:23:26 +0200

    Footer

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+/*
+* File: app.js
+* Author: Pásztor
+* Copyright: 2023, Pásztor
+* Group: Szoft I-2 N
+* Date: 2023-05-19
+* Github: https://github.com/erosistvan/
+* Licenc: GNU GPL
+*/
+
+
 const doc = {
     tbody: document.querySelector('#tbody')
 };
